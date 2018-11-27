418fc9fa5619514dcf78a848328db758a7a640ac
njenekar <nitin.jenekar@gmail.com>
2018-11-28 01:38:07 +0530

    VS Code console application read/write to CosmosDB

---
+++ AzureLearning-module/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+namespace AzureLearning_module
+{
+    class Program
+    {
+        private DocumentClient client;
+        static void Main(string[] args)
+        {
+            try
+{
+    Program p = new Program();
+    p.BasicOperations().Wait();
+}
+catch (DocumentClientException de)
+{
+    Exception baseException = de.GetBaseException();
+    Console.WriteLine("{0} error occurred: {1}, Message: {2}", de.StatusCode, de.Message, baseException.Message);
+}
+catch (Exception e)
+{
+    Exception baseException = e.GetBaseException();
+    Console.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
+}
+finally
+{
+    Console.WriteLine("End of demo, press any key to exit.");
+    Console.ReadKey();
+}
+        }
+
+        private async Task BasicOperations()
+        {
+            this.client = new DocumentClient(new Uri(ConfigurationManager.AppSettings["accountEndpoint"]), ConfigurationManager.AppSettings["accountKey"]);
+            await this.client.CreateDatabaseIfNotExistsAsync(new Database { Id = "Users" });
+            await this.client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri("Users"), new DocumentCollection { Id = "WebCustomers" });
+            Console.WriteLine("Database and collection validation complete");
+        }
+    }
+}
